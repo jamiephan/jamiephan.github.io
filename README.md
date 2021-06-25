340fe4d513ca35c58fabf0bd6472b32b8b936de0
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-06-25 12:35:59 +0100

    Init static web project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jamie Phan's Profile Page
+
+## Start Server:
+
+```npm start```
+
+## Build Page:
+
+```npm run build```
+
+## Development:
+
+```npm run watch``` (Web Browser will be started automatically)
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      hash: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: false,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  devServer: {
+    port: 5000,
+    contentBase: path.join(__dirname, 'dist'),
+    progress: true,
+    liveReload: true,
+    open: true
+  }
+}
